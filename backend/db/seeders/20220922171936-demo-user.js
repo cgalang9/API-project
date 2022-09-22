cf1a63cf50363d7773d052e9193d0aa4096fb0fb
cgalang <cgalang11@hotmail.com>
2022-09-22 10:26:14 -0700

    Add Users table migration and seeds

---
+++ backend/db/seeders/20220922171936-demo-user.js
@@ -0,0 +1,41 @@
+'use strict';
+const bcrypt = require("bcryptjs");
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('Users', [
+      {
+        email: 'demo@user.io',
+        username: 'Demo-lition',
+        hashedPassword: bcrypt.hashSync('password')
+      },
+      {
+        email: 'user1@user.io',
+        username: 'FakeUser1',
+        hashedPassword: bcrypt.hashSync('password2')
+      },
+      {
+        email: 'user2@user.io',
+        username: 'FakeUser2',
+        hashedPassword: bcrypt.hashSync('password3')
+      },
+      {
+        email: 'imfake@user.io',
+        username: 'FakeUser3',
+        hashedPassword: bcrypt.hashSync('password4our')
+      },
+      {
+        email: 'cubeman@user.io',
+        username: 'Cubester',
+        hashedPassword: bcrypt.hashSync('password5ive')
+      }
+    ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete('Users', {
+      username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2', 'FakeUser3', 'Cubester'] }
+    }, {});
+  }
+};
