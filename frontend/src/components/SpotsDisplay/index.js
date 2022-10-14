f10a5c1e460c0a7d64450948c197264a39f7a8e2
cgalang <cgalang11@hotmail.com>
2022-10-14 13:32:11 -0700

    Add getAllSpots action and component

---
+++ frontend/src/components/SpotsDisplay/index.js
@@ -0,0 +1,11 @@
+import './SpotsDisplay.css'
+
+function SpotsDisplay() {
+    return (
+        <div className='spots_display_container'>
+            <h1>spots</h1>
+        </div>
+    )
+}
+
+export default SpotsDisplay
---
+++ frontend/src/store/spots.js
@@ -0,0 +1,30 @@
+import { csrfFetch } from "./csrf"
+
+const GET_ALL_SPOTS = 'spots/GET_ALL_SPOTS'
+
+const getAllSpots = (spots) => {
+    return { type: GET_ALL_SPOTS, spots }
+}
+
+export const getAllSpotsThunk = () => async (dispatch) => {
+    const response = await csrfFetch('/api/spots')
+
+    if(response.ok) {
+        const data = await response.json()
+        dispatch(getAllSpots(data))
+        return data
+    }
+}
+
+const initialState = {}
+
+export const spotsReducer = (state = initialState, action) => {
+    switch(action.type) {
+        case GET_ALL_SPOTS:
+            const newStateGetAll = { ...state }
+            newStateGetAll["spots"] = action.spots
+            return newStateGetAll
+        default:
+            return state
+    }
+}
