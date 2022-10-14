701ac091b0d7eb03ad1300a5d333f791cf6a0337
cgalang <cgalang11@hotmail.com>
2022-10-14 09:44:47 -0700

    Add navigation component and adjusted css

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -11,7 +11,7 @@ function LoginFormPage() {
   const [password, setPassword] = useState('');
   const [errors, setErrors] = useState([]);
 
-  if (sessionUser.id) return (
+  if (sessionUser) return (
     <Redirect to="/" />
   );
 
---
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch } from 'react-redux';
+import * as sessionActions from '../../store/session';
+
+function ProfileButton({ user }) {
+  const dispatch = useDispatch();
+  const [showMenu, setShowMenu] = useState(false);
+
+  const openMenu = () => {
+    if (showMenu) return;
+    setShowMenu(true);
+  };
+
+  useEffect(() => {
+    if (!showMenu) return;
+
+    const closeMenu = () => {
+      setShowMenu(false);
+    };
+
+    document.addEventListener('click', closeMenu);
+
+    return () => document.removeEventListener("click", closeMenu);
+  }, [showMenu]);
+
+  const logout = (e) => {
+    e.preventDefault();
+    dispatch(sessionActions.logout());
+  };
+
+  return (
+    <div className="user_icon_container">
+      <button onClick={openMenu} className="user_btn">
+        <span className="menu_icon"> <i class="fa-sharp fa-solid fa-bars" /></span>
+        <i className="fas fa-user-circle" />
+      </button>
+      {showMenu && (
+        <ul className="profile-dropdown">
+          <li>{user.username}</li>
+          <li>{user.email}</li>
+          <li className='logout_btn_container'>
+            <button onClick={logout} className='logout_btn'>Log Out</button>
+          </li>
+        </ul>
+      )}
+    </div>
+  );
+}
+
+export default ProfileButton;
