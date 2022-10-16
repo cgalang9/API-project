a2a80d4ffcc92350237bd0acf2e7f96336681675
cgalang <cgalang11@hotmail.com>
2022-10-15 17:22:37 -0700

    Add get all reviews by spot id  action/thunk/reducer

--- frontend/src/store/reviews.js
+++ frontend/src/store/reviews.js
@@ -1,9 +1,28 @@
 import { csrfFetch } from "./csrf"
 
+//Get all Reviews by a Spot's id
+const GET_ALL_REVIEWS = 'reviews/GET_ALL_REVIEWS'
+export const getAllReviews = (reviews) => {
+    return { type: GET_ALL_REVIEWS, reviews }
+}
+
+export const getAllReviewsThunk = (spotId) => async (dispatch) => {
+    const response = await csrfFetch(`/api/spots/${spotId}/reviews`)
+
+    if(response.ok) {
+        const reviews = await response.json()
+        dispatch(getAllReviews(reviews))
+        return reviews
+    }
+}
+
 const initialState = {}
 
 export const reviewsReducer = (state = initialState, action) => {
     switch(action.type) {
+        case GET_ALL_REVIEWS:
+            const stateGetAllReviews = { ...action.reviews }
+            return stateGetAllReviews
         default:
             return state
     }
