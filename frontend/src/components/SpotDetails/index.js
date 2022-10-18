b27f0c59f2c9ea038e35f1a9f1df12c9c77b28e3
cgalang <cgalang11@hotmail.com>
2022-10-18 01:06:06 -0700

    Add styling and placement to header and spot images

--- frontend/src/components/SpotDetails/index.js
+++ frontend/src/components/SpotDetails/index.js
@@ -21,35 +21,104 @@ function SpotDetails() {
         isOwner = (true)
     }
 
+    let prevImgUrl = ''
+    let otherImgsArr = []
+    if(spot && spot.SpotImages) {
+        spot.SpotImages.forEach(img => {
+            if(img.preview === true) prevImgUrl = img.url
+            if(img.preview === false) {
+                otherImgsArr.push(img.url)
+            }
+        });
+        while (otherImgsArr.length < 4) {
+            otherImgsArr.push(' ')
+        }
+    }
+
     return(
-        <>
-        {spot && (
+        <div className='spot_details_page'>
+        {spot && spot.SpotImages && (
             <div className='spot_details_container'>
-                <h1>{spot.name}</h1>
-                <div>{spot.address}</div>
-                <div>{spot.city}, {spot.city}</div>
-                <div>{spot.country}</div>
+                <div className='details_head'>{spot.name}</div>
+                <div className='details_subheader'>
+                    <div><i className="fa-sharp fa-solid fa-star"/> {spot.avgStarRating.toFixed(2)}</div>
+                    <div style={{ fontSize: 2 }}><i className="fa-solid fa-circle"/></div>
+                    <div className='underline'>{spot.numReviews} reviews</div>
+                    <div style={{ fontSize: 2 }}><i className="fa-solid fa-circle"/></div>
+                    <div className='underline'>{spot.city}, {spot.state}, {spot.country}</div>
+                </div>
+                <div id='details_img_container'>
+                    <img
+                        src={prevImgUrl}
+                        alt="spot"
+                        className='main_spot_img'
+                        onError={e => {
+                            e.target.src = "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg?20200913095930"
+                            e.onerror = null
+                        }}
+                    />
+                    <div className='other_imgs_container'>
+                        <div id='imgs_row_1'>
+                            <img
+                                src={otherImgsArr[0]}
+                                alt="spot"
+                                className='other_spot_img'
+                                onError={e => {
+                                    e.target.src = "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg?20200913095930"
+                                    e.onerror = null
+                                }}
+                            />
+                            <img
+                                src={otherImgsArr[1]}
+                                alt="spot"
+                                className='other_spot_img top_right'
+                                onError={e => {
+                                    e.target.src = "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg?20200913095930"
+                                    e.onerror = null
+                                }}
+                            />
+                        </div>
+                        <div id='imgs_row_2'>
+                            <img
+                                src={otherImgsArr[2]}
+                                alt="spot"
+                                className='other_spot_img'
+                                onError={e => {
+                                    e.target.src = "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg?20200913095930"
+                                    e.onerror = null
+                                }}
+                            />
+                            <img
+                                src={otherImgsArr[3]}
+                                alt="spot"
+                                className='other_spot_img bottom_right'
+                                onError={e => {
+                                    e.target.src = "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg?20200913095930"
+                                    e.onerror = null
+                                }}
+                            />
+                        </div>
+                        {/* {spot.SpotImages && (
+                            spot.SpotImages.map(img => (
+                                <img
+                                    key={img.id}
+                                    src={img.url}
+                                    alt="spot"
+                                    className='other_spot_img'
+                                    onError={e => {
+                                        e.target.src = "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg?20200913095930"
+                                        e.onerror = null
+                                    }}
+                                />
+                            ))
+                        )} */}
+                    </div>
+                </div>
                 <div>{spot.description}</div>
                 {isOwner && (
                     <div>(<NavLink to={`/spots/${spotId}/edit`}>Edit/Delete Listing</NavLink>)</div>
                 )}
                 <div>${spot.price}</div>
-                <div>{spot.numReviews} reviews</div>
-                <div><i className="fa-sharp fa-solid fa-star"/>{spot.avgStarRating}</div>
-                {spot.SpotImages && (
-                    spot.SpotImages.map(img => (
-                        <img
-                            key={img.id}
-                            src={img.url}
-                            alt="spot"
-                            className='spot_details_img'
-                            onError={e => {
-                                e.target.src = "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg?20200913095930"
-                                e.onerror = null
-                            }}
-                        />
-                    ))
-                )}
                 <div className='reviews_header'>
                     <div><i className="fa-sharp fa-solid fa-star"/>{spot.avgStarRating}</div>
                     <div style={{ fontSize: 3 }}><i className="fa-solid fa-circle"/></div>
@@ -58,7 +127,7 @@ function SpotDetails() {
                 <Reviews />
             </div>
         )}
-        </>
+        </div>
     )
 }
 
