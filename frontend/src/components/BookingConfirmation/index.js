2017c75aad8e00ceeba8f358cb3a0b94171e6ae6
cgalang <cgalang11@hotmail.com>
2022-12-02 10:46:07 -0800

    Add redirect of booking confirmed page if not booking information passed to component

--- frontend/src/components/BookingConfirmation/index.js
+++ frontend/src/components/BookingConfirmation/index.js
@@ -1,22 +1,32 @@
-import { useLocation, NavLink } from "react-router-dom"
+import { useEffect } from "react"
+import { useLocation, NavLink, useHistory } from "react-router-dom"
 import './BookingConfirmation.css'
 
 function BookingConfirmation() {
     const location = useLocation()
-    const startDate = new Date(location.state.newBooking.startDate.replace(/-/g, '\/')).toDateString()
-    const endDate = new Date(location.state.newBooking.endDate.replace(/-/g, '\/')).toDateString()
+    const history = useHistory()
+    let startDate;
+    let endDate;
 
-    console.log(location)
+    useEffect(() => {
+        if (!location.state) {
+            history.push('/')
+        } else {
+            startDate = new Date(location.state.newBooking.startDate.replace(/-/g, '\/')).toDateString()
+            endDate = new Date(location.state.newBooking.endDate.replace(/-/g, '\/')).toDateString()
+        }
+    }, [])
+
     return (
         <div className="booking_confirmed_wrapper">
-            {/* <div className="booking_confirmed_container">
+            <div className="booking_confirmed_container">
                 <div className="booking_confirmed_left">
                     <h1>Confirm and pay</h1>
                 </div>
                 <div className="booking_confirmed_right">
                     <h1>test</h1>
-                </div> */}
-                <h1 className="top">You Reservation Has Been Successfully Created</h1>
+                </div>
+                {/* <h1 className="top">You Reservation Has Been Successfully Created</h1>
                 <div className="booking_location_confirmed">Location: {location.state.name}</div>
                 <div className="booking_dates_confirmed">Dates: {startDate} - {endDate}</div>
                 <div className="booking_location_confirmed">{location.state.guests} {location.state.guests === 1 ? 'Guest' : 'Guests'} </div>
@@ -26,8 +36,8 @@ function BookingConfirmation() {
                     <div className='service_fee_confirmed'>Service Fee = ${location.state.serviceFee}</div>
                     <div className="total_fee_confirmed">Total = ${(location.state.price * location.state.bookingLength) + location.state.cleaningFee + location.state.serviceFee}</div>
                 </div>
-                <div style={{ marginTop: 25 }}><NavLink exact to="/current-user/bookings">Go To Your Bookings</NavLink></div>
-            {/* </div> */}
+                <div style={{ marginTop: 25 }}><NavLink exact to="/current-user/bookings">Go To Your Bookings</NavLink></div> */}
+            </div>
         </div>
     )
 }
