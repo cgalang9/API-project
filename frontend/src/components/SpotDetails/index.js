fd732f27768f7924a153e2632b2bc8c03dea2713
cgalang <cgalang11@hotmail.com>
2022-12-22 10:41:03 -0800

    Clear spot data when exiting spot details page

--- frontend/src/components/SpotDetails/index.js
+++ frontend/src/components/SpotDetails/index.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { NavLink, useHistory, useParams } from "react-router-dom";
-import { getSpotThunk } from "../../store/currentSpot";
+import { getSpotThunk, clearSpot } from "../../store/currentSpot";
 import CreateBookingTile from "../CreateBookingTile";
 import Reviews from "../Reviews";
 import "./SpotDetails.css";
@@ -42,6 +42,12 @@ function SpotDetails() {
       otherImgsArr.push(error_img);
     }
   }
+
+  useEffect(() => {
+    return () => {
+      dispatch(clearSpot());
+    };
+  }, []);
 
   return (
     <div className="spot_details_page">
--- frontend/src/store/currentSpot.js
+++ frontend/src/store/currentSpot.js
@@ -1,55 +1,59 @@
-import { csrfFetch } from "./csrf"
-
+import { csrfFetch } from "./csrf";
 
 //get spot detail by id
-const GET_SPOT = 'spots/GET_SPOT'
+const GET_SPOT = "spots/GET_SPOT";
 const getSpot = (spot) => {
-    return { type: GET_SPOT, spot }
-}
+  return { type: GET_SPOT, spot };
+};
 
 export const getSpotThunk = (spotId) => async (dispatch) => {
-    const response = await csrfFetch(`/api/spots/${spotId}`)
+  const response = await csrfFetch(`/api/spots/${spotId}`);
 
-    if(response.ok) {
-        const spot = await response.json()
-        dispatch(getSpot(spot))
-        return spot
-    }
-
-}
+  if (response.ok) {
+    const spot = await response.json();
+    dispatch(getSpot(spot));
+    return spot;
+  }
+};
 
 //Add image to spot by spot id
-const ADD_SPOT_IMG = 'spots/ADD_SPOT_IMG'
+const ADD_SPOT_IMG = "spots/ADD_SPOT_IMG";
 const addSpotImg = (spot) => {
-    return { type: ADD_SPOT_IMG, spot }
-}
+  return { type: ADD_SPOT_IMG, spot };
+};
 
 export const addSpotImgThunk = (spotId, imgUrl) => async (dispatch) => {
-    const response = await csrfFetch(`/api/spots/${spotId}/images` , {
-        method: 'POST',
-        body: JSON.stringify({
-            url: imgUrl,
-            preview: false
-        })
-    })
+  const response = await csrfFetch(`/api/spots/${spotId}/images`, {
+    method: "POST",
+    body: JSON.stringify({
+      url: imgUrl,
+      preview: false,
+    }),
+  });
 
-    if(response.ok) {
-        const img = await response.json()
-        dispatch(addSpotImg(img))
-        return img
-    }
+  if (response.ok) {
+    const img = await response.json();
+    dispatch(addSpotImg(img));
+    return img;
+  }
+};
 
-}
-
+//Clear spot
+const CLEAR_SPOT = "spots/CLEAR_SPOT";
+export const clearSpot = () => {
+  return { type: CLEAR_SPOT };
+};
 
 export const currentSpotReducer = (state = null, action) => {
-    switch(action.type) {
-        case GET_SPOT:
-            const stateGetSpot = {...action.spot}
-            return stateGetSpot
-        case ADD_SPOT_IMG:
-            return state
-        default:
-            return state
-    }
-}
+  switch (action.type) {
+    case GET_SPOT:
+      const stateGetSpot = { ...action.spot };
+      return stateGetSpot;
+    case ADD_SPOT_IMG:
+      return state;
+    case CLEAR_SPOT:
+      return null;
+    default:
+      return state;
+  }
+};
